b4fa2a535fd01b38833c6b688041ffcc5d4e5880
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-14 10:29:15 -0300

    feat: create new block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,24 @@ function Blockchain() {
   this.chain = []; // chain of blocks
   this.newTransactions = []; // hold all of new transactions before put in a block
 }
+
+// Create a New Block
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1, // block number
+    timestamp: Date.now(),
+    transactions: this.newTransactions, // pending transactions
+    nonce: nonce, // number, proof that we created this block with proof of work
+    hash: hash, // hash of the block's data
+    previousBlockHash: previousBlockHash,
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
