eb807f1f4a4f5b611d978ac6b6c4c61015921b3a
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-15 18:30:39 -0300

    feat: create the mine endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const uuid = require("uuid");
+
 const Blockchain = require("./blockchain");
 
 // Initiate our Blockchain
@@ -7,6 +9,9 @@ const maua = new Blockchain();
 const app = express();
 app.use(express.json());
 
+// Create Miner'address
+const nodeAddress = uuid.v1().split("-").join("");
+
 // Get Blockchain
 app.get("/blockchain", (req, res) => {
   res.json(maua);
@@ -14,15 +19,33 @@ app.get("/blockchain", (req, res) => {
 
 // Create a Transactions
 app.post("/transactions", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { amount, sender, recipient } = req.body;
 
-  const blockIndex = maua.createNewTransaction(sender, recipient, amount);
+  const blockIndex = maua.createNewTransaction(amount, sender, recipient);
 
   res.json({ blockIndex });
 });
 
 // Create a Block
-app.get("/mine", (req, res) => {});
+app.get("/mine", (req, res) => {
+  const lastBlock = maua.getLastBlock();
+  const previousHash = lastBlock["hash"];
+
+  const currentBlockData = {
+    transactions: maua.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+
+  const nonce = maua.proofOfWork(previousHash, currentBlockData);
+  const currentHash = maua.hashBlock(previousHash, currentBlockData, nonce);
+
+  // Rewarding the miner
+  maua.createNewTransaction(12.5, "0x00", nodeAddress);
+
+  const newBlock = maua.createNewBlock(nonce, previousHash, currentHash);
+
+  res.json(newBlock);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
