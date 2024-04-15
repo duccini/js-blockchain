7a0d538e0e8984d25e23bd08830c56fde150a041
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-15 17:58:59 -0300

    feat: create a new transaction

--- dev/api.js
+++ dev/api.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const Blockchain = require("./blockchain");
+
+// Initiate our Blockchain
+const maua = new Blockchain();
 
 const app = express();
 app.use(express.json());
 
 // Get Blockchain
-app.get("/blockchain", (req, res) => {});
+app.get("/blockchain", (req, res) => {
+  res.json(maua);
+});
 
 // Create a Transactions
 app.post("/transactions", (req, res) => {
-  const transactions = req.body.transactions;
+  const { sender, recipient, amount } = req.body;
 
-  console.log(transactions);
+  const blockIndex = maua.createNewTransaction(sender, recipient, amount);
 
-  res.end();
+  res.json({ blockIndex });
 });
 
 // Create a Block
