6ed95426f3f9ac2d56a1d973187bd336a5541a6c
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-18 12:26:03 -0300

    feat: Create and Broadcast Node

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -53,10 +53,16 @@ app.get("/mine", (req, res) => {
 // Create and Broadcast Node
 app.post("/create-and-broadcast-node", (req, res) => {
   const newNodeUrl = req.body.newNodeUrl;
+
+  // Register current Node to itself
+  if (maua.networkNodes.indexOf(newNodeUrl) == -1)
+    maua.networkNodes.push(newNodeUrl);
 });
 
 // Register a Node
-app.post("/register-node", (req, res) => {});
+app.post("/register-node", (req, res) => {
+  res.end();
+});
 
 // Register Multiple Nodes at Once
 app.post("/register-nodes-bulk", (req, res) => {});
